d6c835cc48341ece1d899a8ce19850aa848ed7d4
smallwisp <4ubarov96@mail.ru>
2024-07-15 02:27:54 +0300

    added slider and fonts

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const swiperThumb = new Swiper('.gift__swiper--thumb', {
+  spaceBetween: 16,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+const swiperMain = new Swiper('.gift__swiper--card', {
+  spaceBetween: 16,
+  thumbs: {
+    swiper: swiperThumb,
+  } 
+});
