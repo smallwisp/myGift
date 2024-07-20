cf417673bfa3e6167a57785236b3486730c1844f
smallwisp <4ubarov96@mail.ru>
2024-07-21 00:32:16 +0300

    slider styled

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const swiperThumb = new Swiper('.gift__swiper--thumb', {
   spaceBetween: 16,
   slidesPerView: 6,
   freeMode: true,
-  watchSlidesProgress: true,
 });
 
 const swiperMain = new Swiper('.gift__swiper--card', {
