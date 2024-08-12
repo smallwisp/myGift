f23fae602d1a0837123a782afa7eb1994e9cec1b
smallwisp <4ubarov96@mail.ru>
2024-08-13 01:31:57 +0300

    added mask and validate form

--- js/script.js
+++ js/script.js
@@ -20,3 +20,45 @@ const swiperMain = new Swiper('.gift__swiper--card', {
 });
 
 const phoneInputs = document.querySelectorAll('.form__field--phone');
+const form = document.querySelector('.form');
+const submitButton = document.querySelector('.form__button');
+
+phoneInputs.forEach((item) => {
+  IMask(item, {
+    mask: "+{7}(000)000-00-00",
+  })
+})
+
+const updateSubmitButton = () => {
+  let isFormFilled = true;
+  for  (const field of form.elements) {
+    if (field.classList.contains('form__field')) {
+      if (!field.value.trim()) {
+        isFormFilled = false;
+        break;
+      }
+    }
+  }
+
+  submitButton.disabled = !isFormFilled;
+};
+
+const phoneValidateOption = {
+  presence: {
+    message: 'Поле телефон обязательно для заполнения!',
+  },
+  format: {
+    pattern: '\\+7\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}'
+  }
+}
+
+form.addEventListener('input', updateSubmitButton)
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const errors = Validate(form, {
+    sender_phone: ,
+    receiver_phone:
+  })
+})
--- js/scriptCard.js
+++ js/scriptCard.js
@@ -28,3 +28,5 @@ const init = () => {
 
 init()
 
+
+
